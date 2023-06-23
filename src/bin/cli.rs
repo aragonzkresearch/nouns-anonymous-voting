9eb7862d0350f709c7afc965cf39b38a74864f08
Artem <48178500+ElusAegis@users.noreply.github.com>
2023-06-23 13:14:25 +0100

    Key Registration Works

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,28 +1,23 @@
 extern crate core;
 
 use std::env;
-use std::hash::Hash;
-use std::io::{self, Write};
 use std::ops::Deref;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
 
-use ark_ff::{BigInt, BigInteger, PrimeField};
 use babyjubjub_ark::PrivateKey;
-use clap::builder::TypedValueParser;
-use clap::{arg, command, Arg, ArgAction, Command};
-use ethers::core::k256::elliptic_curve::bigint::Encoding;
+use clap::{command, Arg, Command};
 use ethers::core::k256::U256;
 use ethers::middleware::SignerMiddleware;
 use ethers::prelude::{abigen, Signer, U256 as EthersU256};
 use ethers::providers::{Http, Middleware, Provider, ProviderExt};
 use ethers::signers::LocalWallet;
-use ethers::types::{Address, NameOrAddress, StorageProof, TransactionRequest};
-use futures::TryStreamExt;
+use ethers::types::{Address, StorageProof};
 
 use nouns_anonymous_voting::voter::Voter;
-use nouns_anonymous_voting::{wrap, wrap_into, BBJJ_Ec, BBJJ_Fr, BN254_Fr, VoteChoice, Wrapper};
+use nouns_anonymous_voting::{wrap, wrap_into, BBJJ_Ec, VoteChoice, Wrapper};
+use parsers::*;
 
 /// The CLI that to interact with the Nouns Anonymous Voting System
 /// As global parameters, it should take:
@@ -65,7 +60,7 @@ use nouns_anonymous_voting::{wrap, wrap_into, BBJJ_Ec, BBJJ_Fr, BN254_Fr, VoteCh
 #[tokio::main]
 async fn main() {
     // parse the CLI input
-    let (global_param, cli_command) = cli().unwrap_or_else(|err| {
+    let (global_param, cli_command) = get_user_input().unwrap_or_else(|err| {
         eprintln!("Error: {}", err);
         std::process::exit(1);
     });
@@ -74,7 +69,16 @@ async fn main() {
     let eth_connection = Provider::<Http>::connect(global_param.rpc_url.as_str()).await;
     // create the signer for the txs
     let wallet = LocalWallet::from_bytes(global_param.tx_private_key.as_ref()).unwrap();
-    let client = SignerMiddleware::new(eth_connection.clone(), wallet);
+    let chain_id = eth_connection
+        .get_chainid()
+        .await
+        .map_err(|_e| "Could not get chain id".to_string())
+        .unwrap();
+
+    let client = SignerMiddleware::new(
+        eth_connection.clone(),
+        wallet.with_chain_id(chain_id.as_u64()),
+    );
 
     match cli_command {
         CliCommand::RegKey(bbjj_private_key) => {
@@ -97,12 +101,6 @@ async fn main() {
             vote_choice,
             tlcs_pbk,
         ) => {
-            let chain_id = eth_connection
-                .get_chainid()
-                .await
-                .map_err(|e| "Could not get chainid".to_string())
-                .unwrap();
-
             vote(
                 client,
                 global_param.contract_address,
@@ -122,16 +120,32 @@ async fn main() {
         eprintln!("Error: {}", err);
         std::process::exit(1);
     });
+
+    println!("Done!");
 }
 
 /// Function that registers a new BBJJ Public Key in the ZKRegistry contract.
 async fn reg_key(
     client: SignerMiddleware<Provider<Http>, LocalWallet>,
-    contract_address: Address,
+    nouns_voting_address: Address,
     bbjj_private_key: PrivateKey,
 ) -> Result<(), String> {
     /// The BBJJ Public Key interface ID in ZKRegistry
     const INTERFACE_ID: u8 = 0x00;
+
+    let client = Arc::new(client);
+
+    abigen!(
+        NounsVoting,
+        r#"[
+            function zkRegistry() view returns (address)
+        ]"#,
+    );
+
+    let nouns_voting = NounsVoting::new(nouns_voting_address, client.clone());
+    let zk_registry_address = nouns_voting.zk_registry().call().await.map_err(|e| {
+        format!("Error getting the ZKRegistry address from the NounsVoting contract: {e:?}")
+    })?;
 
     abigen!(
         ZKRegistry,
@@ -140,8 +154,7 @@ async fn reg_key(
         ]"#,
     );
 
-    let client = Arc::new(client);
-    let contract = ZKRegistry::new(contract_address, client);
+    let contract = ZKRegistry::new(zk_registry_address, client);
 
     let bbjj_pbk = bbjj_private_key.public();
     let bbjj_pbk: [U256; 2] = wrap_into!(bbjj_pbk);
@@ -151,7 +164,7 @@ async fn reg_key(
     let x_tx = x_register_request
         .send()
         .await
-        .map_err(|e| format!("Error sending X coordinate registration tx"))?;
+        .map_err(|e| format!("Error sending X coordinate registration tx {e:?}"))?;
     println!(
         "Registered X Part on of the BBJJ Public Key  Interface. Tx Hash: {}",
         x_tx.tx_hash()
@@ -162,7 +175,7 @@ async fn reg_key(
     let y_tx = y_register_request
         .send()
         .await
-        .map_err(|e| format!("Error sending Y coordinate registration tx"))?;
+        .map_err(|_e| format!("Error sending Y coordinate registration tx"))?;
 
     println!(
         "Registered Y Part on of the BBJJ Public Key Interface. Tx Hash: {}",
@@ -208,7 +221,7 @@ async fn create_process(
     let tx = create_process_request
         .send()
         .await
-        .map_err(|e| format!("Error sending createProcess tx"))?;
+        .map_err(|_e| format!("Error sending createProcess tx"))?;
 
     println!("Tx Hash: {}", tx.tx_hash());
     println!("Process created successfully!");
@@ -279,7 +292,7 @@ async fn vote(
     let tx = submit_vote_request
         .send()
         .await
-        .map_err(|e| format!("Error sending vote tx"))?;
+        .map_err(|_e| format!("Error sending vote tx"))?;
 
     println!("Tx Hash: {}", tx.tx_hash());
     println!("Vote submitted successfully!");
@@ -304,13 +317,13 @@ enum CliCommand {
     None, // No command was chosen
 }
 
-fn cli() -> Result<(GlobalCliParams, CliCommand), String> {
+fn get_user_input() -> Result<(GlobalCliParams, CliCommand), String> {
     let command = command_constructor();
     let matches = command.get_matches();
 
     // Parse the global parameters
     let global_cli_param = {
-        let contract_address = matches
+        let contract_address: &String = matches
             .get_one("contract-address")
             .ok_or("Missing contract address")?;
         let rpc_url: &String = matches.get_one("rpc-url").ok_or("Missing RPC URL")?;
@@ -318,7 +331,7 @@ fn cli() -> Result<(GlobalCliParams, CliCommand), String> {
             .get_one("tx-private-key")
             .ok_or("Missing transaction private key")?;
 
-        let contract_address = Address::from_str(*contract_address)
+        let contract_address = Address::from_str(contract_address)
             .map_err(|e| format!("Invalid contract address: {}", e))?;
         let tx_private_key =
             hex::decode(tx_private_key).map_err(|e| format!("Invalid private key: {}", e))?;
@@ -334,11 +347,11 @@ fn cli() -> Result<(GlobalCliParams, CliCommand), String> {
 
     if let Some(matches) = matches.subcommand_matches("reg-key") {
         let key_to_reg: &String = matches
-            .get_one("key_to_reg")
+            .get_one("reg-private-key")
             .ok_or("Missing key to register in ZKRegistry")?;
 
         let key_to_reg =
-            parse_bbjj_prk(key_to_reg).map_err(|e| "Invalid key to register in ZKRegistry")?;
+            parse_bbjj_prk(key_to_reg).map_err(|_e| "Invalid key to register in ZKRegistry")?;
 
         return Ok((global_cli_param, CliCommand::RegKey(key_to_reg)));
     }
@@ -385,7 +398,7 @@ fn cli() -> Result<(GlobalCliParams, CliCommand), String> {
         let nft_owner_address = Address::from_str(*nft_owner_address)
             .map_err(|e| format!("Invalid nft owner address: {}", e))?;
         let nft_owner_prk =
-            parse_bbjj_prk(nft_owner_prk).map_err(|e| "Invalid nft owner private key")?;
+            parse_bbjj_prk(nft_owner_prk).map_err(|_e| "Invalid nft owner private key")?;
         let vote_choice = VoteChoice::from(vote_choice.as_str());
         let tlcs_pbk = parse_tlcs_pbk(tlcs_pbk)?;
 
@@ -410,7 +423,7 @@ fn cli() -> Result<(GlobalCliParams, CliCommand), String> {
             .ok_or("Missing tcls public key")?;
 
         let process_id = parse_u256(process_id)?;
-        let tcls_prk = parse_bbjj_prk(tcls_prk).map_err(|e| "Invalid tcls private key")?;
+        let tcls_prk = parse_bbjj_prk(tcls_prk).map_err(|_e| "Invalid tcls private key")?;
 
         return Ok((global_cli_param, CliCommand::Tally(process_id, tcls_prk)));
     }
@@ -419,83 +432,93 @@ fn cli() -> Result<(GlobalCliParams, CliCommand), String> {
     Ok((global_cli_param, CliCommand::None))
 }
 
-/// Parses a hex string into BBJJ PrivateKey
-/// Example: `1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef` of 32 bytes
-fn parse_bbjj_prk(key_to_reg: &String) -> Result<PrivateKey, String> {
-    PrivateKey::import(
-        hex::decode(key_to_reg).map_err(|e| format!("Failed to parse hex string: {}", e))?,
-    )
-}
+mod parsers {
+    use std::time::Duration;
 
-/// Parses a duration string into a Duration
-/// Example: `1d` (1 day)
-/// Example: `1h` (1 hour)
-/// Example: `1m` (1 minute)
-fn parse_duration<T: Into<String>>(s: T) -> Duration {
-    let s = s.into();
-    let mut chars = s.chars();
-    let mut number = String::new();
-    let mut unit = String::new();
-    while let Some(c) = chars.next() {
-        if c.is_numeric() {
-            number.push(c);
+    use ark_ff::PrimeField;
+    use babyjubjub_ark::PrivateKey;
+    use ethers::core::k256::U256;
+
+    use nouns_anonymous_voting::{BBJJ_Ec, BN254_Fr};
+
+    /// Parses a hex string into BBJJ PrivateKey
+    /// Example: `1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef` of 32 bytes
+    pub(crate) fn parse_bbjj_prk(key_to_reg: &String) -> Result<PrivateKey, String> {
+        PrivateKey::import(
+            hex::decode(key_to_reg).map_err(|e| format!("Failed to parse hex string: {}", e))?,
+        )
+    }
+
+    /// Parses a duration string into a Duration
+    /// Example: `1d` (1 day)
+    /// Example: `1h` (1 hour)
+    /// Example: `1m` (1 minute)
+    pub(crate) fn parse_duration<T: Into<String>>(s: T) -> Duration {
+        let s = s.into();
+        let mut chars = s.chars();
+        let mut number = String::new();
+        let mut unit = String::new();
+        while let Some(c) = chars.next() {
+            if c.is_numeric() {
+                number.push(c);
+            } else {
+                unit.push(c);
+            }
+        }
+        let number = number.parse::<u64>().unwrap();
+        let duration = match unit.as_str() {
+            "d" => Duration::from_secs(number * 24 * 60 * 60),
+            "h" => Duration::from_secs(number * 60 * 60),
+            "m" => Duration::from_secs(number * 60),
+            _ => panic!("Invalid duration unit"),
+        };
+        duration
+    }
+
+    /// Parses a be TLCS Public Key string into a BBJJ_Ec
+    pub(crate) fn parse_tlcs_pbk<T: Into<String>>(s: T) -> Result<BBJJ_Ec, String> {
+        let s = s.into();
+        let mut chars = s.chars();
+        let mut x = String::new();
+        let mut y = String::new();
+        while let Some(c) = chars.next() {
+            if c == ',' {
+                break;
+            }
+            x.push(c);
+        }
+        while let Some(c) = chars.next() {
+            y.push(c);
+        }
+
+        let x = BN254_Fr::from_be_bytes_mod_order(
+            &hex::decode(x).map_err(|_| "Invalid TLCS Public Key X coordinate")?,
+        );
+        let y = BN254_Fr::from_be_bytes_mod_order(
+            &hex::decode(y).map_err(|_| "Invalid TLCS Public Key Y coordinate")?,
+        );
+
+        Ok(BBJJ_Ec { x, y })
+    }
+
+    /// Parses a U256
+    /// Supports both decimal (for small numbers) and hex (for large numbers) representations
+    /// Decimal example: `123456789`, the size of the number is limited to 8 bytes
+    /// Hex example: `0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef` (be 32 bytes)
+    pub(crate) fn parse_u256<T: Into<String>>(s: T) -> Result<U256, String> {
+        let s = s.into();
+
+        // Check if the string starts with 0x
+        return if s[0..2] == "0x".as_ref() {
+            // If it does, we parse it as a hex string
+            let s = &s[2..];
+            Ok(U256::from_be_hex(s))
         } else {
-            unit.push(c);
-        }
+            // If it doesn't, we parse it as a decimal string
+            let number = s.parse::<u64>().map_err(|_| "Invalid decimal string")?;
+            Ok(U256::from(number))
+        };
     }
-    let number = number.parse::<u64>().unwrap();
-    let duration = match unit.as_str() {
-        "d" => Duration::from_secs(number * 24 * 60 * 60),
-        "h" => Duration::from_secs(number * 60 * 60),
-        "m" => Duration::from_secs(number * 60),
-        _ => panic!("Invalid duration unit"),
-    };
-    duration
-}
-
-/// Parses a be TLCS Public Key string into a BBJJ_Ec
-fn parse_tlcs_pbk<T: Into<String>>(s: T) -> Result<BBJJ_Ec, String> {
-    let s = s.into();
-    let mut chars = s.chars();
-    let mut x = String::new();
-    let mut y = String::new();
-    while let Some(c) = chars.next() {
-        if c == ',' {
-            break;
-        }
-        x.push(c);
-    }
-    while let Some(c) = chars.next() {
-        y.push(c);
-    }
-
-    let x = BN254_Fr::from_be_bytes_mod_order(
-        &hex::decode(x).map_err(|_| "Invalid TLCS Public Key X coordinate")?,
-    );
-    let y = BN254_Fr::from_be_bytes_mod_order(
-        &hex::decode(y).map_err(|_| "Invalid TLCS Public Key Y coordinate")?,
-    );
-
-    Ok(BBJJ_Ec { x, y })
-}
-
-/// Parses a U256
-/// Supports both decimal (for small numbers) and hex (for large numbers) representations
-/// Decimal example: `123456789`, the size of the number is limited to 8 bytes
-/// Hex example: `0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef` (be 32 bytes)
-fn parse_u256<T: Into<String>>(s: T) -> Result<U256, String> {
-    let s = s.into();
-
-    // Check if the string starts with 0x
-    return if s[0..2] == "0x".as_ref() {
-        // If it does, we parse it as a hex string
-        let s = &s[2..];
-        Ok(U256::from_be_hex(s))
-    } else {
-        // If it doesn't, we parse it as a decimal string
-        let number = s.parse::<u64>().map_err(|_| "Invalid decimal string")?;
-        Ok(U256::from(number))
-    };
 }
 
 /// Constructs the CLI
@@ -634,15 +657,3 @@ fn command_constructor() -> Command {
                 ),
         )
 }
-
-// fn voter() {
-//     // Storage Prove that the voter `address` is mapped to a `pbk` in the registry
-//     // We later proof that the `pbk` is the Public Key corresponding to the voters `sk`
-//     let registry_key_sp = StateProof::default(); // TODO - get real value
-//
-//     // Storage Prove that the voter's address owns the NFT
-//     let nft_ownership_proof = StateProof::default(); // TODO - get real value
-//
-//     // Storage Prove that the NFT is or is not delegated
-//     // let nft_delegation_proof =  StateProof::default(); // TODO - enable delegation in the future
-// }
