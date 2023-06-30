efd7343dbf801ecf53081b202b485f00ef5fcc06
Ahmad <ahmad.afuni@gmail.com>
2023-06-30 17:59:33 +1000

    Remove dupe

--- nouns-protocol/src/utils/mock.rs
+++ nouns-protocol/src/utils/mock.rs
@@ -14,14 +14,6 @@ pub trait Mock {
     fn mock<R: Rng>(rng: &mut R) -> Self;
 }
 
-impl Mock for PrivateKey {
-    fn mock<R: Rng>(rng: &mut R) -> Self {
-        let mut RK_i = vec![0u8; 32];
-        rng.fill_bytes(&mut RK_i);
-        PrivateKey::import(RK_i).expect("Could not generate a mock BBJJ Private Key.")
-    }
-}
-
 impl Mock for Voter {
     fn mock<R: Rng>(rng: &mut R) -> Self {
         // Generate a random Vec of bytes length 32
@@ -106,6 +98,6 @@ impl Mock for PrivateKey {
         let mut bytes = [0u8; 32];
         rng.fill_bytes(&mut bytes);
 
-        PrivateKey::import(bytes.to_vec()).unwrap()
+        PrivateKey::import(bytes.to_vec()).expect("Could not generate a mock BBJJ Private Key.")
     }
 }
