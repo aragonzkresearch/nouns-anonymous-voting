738923444f7564fa78cd9fafc00adbd3eba5fbff
Ahmad <ahmad.afuni@gmail.com>
2023-08-04 15:25:10 +1000

    Fix contract prep script

--- prep-contracts.sh
+++ prep-contracts.sh
@@ -7,7 +7,8 @@ generate_contract() {
 	nargo codegen-verifier
 }
 copy_contract() {
-	FILE_STR=$(sed -e "s/UltraVerificationKey/$1UltraVerificationKey/g" -e '6iimport "./BaseUltraVerifier.sol";\n' contract/plonk_vk.sol)
+        PKG_NAME=$(grep -o 'name.*=.*".*"' Nargo.toml | sed -e 's/name.*=.*\s//g' -e 's/"//g')
+	FILE_STR=$(sed -e "s/UltraVerificationKey/$1UltraVerificationKey/g" -e '6iimport "./BaseUltraVerifier.sol";\n' contract/$PKG_NAME/plonk_vk.sol)
 	echo "$FILE_STR" | head -n 75 > ../../contracts/src/$1Verifier.sol
 	echo "contract $1Verifier is BaseUltraVerifier {" >> ../../contracts/src/$1Verifier.sol
 	echo "$FILE_STR" | tail -n 8 >> ../../contracts/src/$1Verifier.sol
